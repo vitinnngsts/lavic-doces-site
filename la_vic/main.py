165bd6baa3cb774b2fd51dafee7f5f61c8c8093d
Vithor <vitinnngsts@gmail.com>
2026-03-08 19:55:00 -0300

    site La Vic Doces

---
+++ la_vic/main.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, request
+from la_vic.produtos import CARDAPIO_DOCERIA
+
+app = Flask(__name__)
+
+@app.route('/')
+def homepage():
+    return render_template(
+        'homepage.html',
+        cardapio=CARDAPIO_DOCERIA,
+    )
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ la_vic/produtos.py
@@ -0,0 +1,55 @@
+
+PRODUTOS_BOMBOM = [
+    {"id": 1, "nome": "trufa de Morango", "descricao": "Bombom recheado sabor morango", "preco": 2.50, "imagem": "trufa_morango.jpg"},
+    {"id": 2, "nome": "trufa de Maracujá", "descricao": "Bombom recheado sabor maracujá", "preco": 2.50, "imagem": "trufa_maracuja.jpg"},
+    {"id": 3, "nome": "trufa de Limão", "descricao": "Bombom recheado sabor limão", "preco": 2.50, "imagem": "trufa_limao.jpg"},
+    {"id": 4, "nome": "trufa de Brigadeiro", "descricao": "Bombom recheado com brigadeiro", "preco": 2.50, "imagem": "trufa_tradicional.jpg"},
+    {"id": 5, "nome": "trufa de Coco", "descricao": "Bombom recheado sabor coco", "preco": 2.50, "imagem": "trufa_beijinho.jpg"},
+   # {"id": 6, "nome": "trufa de Leite Ninho", "descricao": "Bombom gourmet de leite ninho", "preco": 2.50, "imagem": "bombom_ninho.png"},
+   # {"id": 7, "nome": "trufa de Leite Ninho com Nutella", "descricao": "Bombom de leite ninho com recheio de Nutella", "preco": 2.50, "imagem": "bombom_ninho_nutella.png", "queridinho": True},
+   # {"id": 8, "nome": "trufa de Oreo", "descricao": "Bombom recheado com creme de Oreo", "preco": 2.50, "imagem": "bombom_oreo.png"}
+]
+
+
+PRODUTOS_BRIGADEIROS = [
+    {"id": 9, "nome": "Brigadeiro Tradicional", "descricao": "Brigadeiro clássico com granulado", "preco": 2.50, "imagem": "tradicional.jpg"},
+    {"id": 10, "nome": "Beijinho", "descricao": "Brigadeiro branco com coco", "preco": 2.50, "imagem": "beijinho.jpg"},
+    {"id": 11, "nome": "Brigadeiro de Ninho", "descricao": "Brigadeiro gourmet de leite ninho", "preco": 2.50, "imagem": "ninho.jpg"},
+    {"id": 12, "nome": "Brigadeiro Ninho com Nutella", "descricao": "Brigadeiro de leite ninho com Nutella", "preco": 2.50, "imagem": "ninho_nutela.jpg", "queridinho": True},
+    {"id": 13, "nome": "Brigadeiro de Casadinho", "descricao": "", "preco": 2.50, "imagem": "casadinho.jpg"},
+    {"id": 14, "nome": "Brigadeiro de Caramelo Salgado", "descricao": "Brigadeiro gourmet com caramelo salgado", "preco":2.50, "imagem": "sucrilhos.jpg"},
+    {"id": 15, "nome": "Brigadeiro de Oreo", "descricao": "Brigadeiro gourmet com Oreo", "preco": 2.50, "imagem": "oreo.jpg"},
+    {"id": 16, "nome": "Brigadeiro de Maracujá", "descricao": "Brigadeiro gourmet sabor maracujá", "preco": 2.50, "imagem": "maracuja.jpg"},
+    {"id": 17, "nome": "Brigadeiro paçoca", "descricao": "Brigadeiro inspirado no Ferrero Rocher", "preco": 2.50, "imagem": "pacoca.jpg", "queridinho": True}
+]
+
+
+PRODUTOS_GELADINHOS = [
+    {"id": 18, "nome": "Geladinho de Maracujá com Casquinha", "descricao": "Geladinho de maracujá com casquinha de chocolate", "preco": 5.00, "imagem": "geladinho_maracuja_casca.png"},
+    {"id": 19, "nome": "Geladinho de Limão com Casquinha", "descricao": "Geladinho de limão com casquinha de chocolate", "preco": 5.00, "imagem": "geladinho_limao_casca.png"},
+    {"id": 20, "nome": "Geladinho de Chocolate", "descricao": "Geladinho gourmet sabor chocolate", "preco": 4.00, "imagem": "geladinho_chocolate.png"},
+    {"id": 21, "nome": "Geladinho de Maracujá", "descricao": "Geladinho tradicional sabor maracujá", "preco": 4.00, "imagem": "geladinho_maracuja.png"},
+    {"id": 22, "nome": "Geladinho de Limão", "descricao": "Geladinho tradicional sabor limão", "preco": 4.00, "imagem": "geladinho_limao.png"},
+    {"id": 23, "nome": "Geladinho de Oreo", "descricao": "Geladinho gourmet de Oreo", "preco": 5.00, "imagem": "geladinho_oreo.png"},
+    {"id": 24, "nome": "Geladinho de Oreo com Nutella", "descricao": "Geladinho de Oreo com Nutella", "preco": 6.00, "imagem": "geladinho_oreo_nutella.png", "queridinho": True},
+    {"id": 25, "nome": "Geladinho de Leite Ninho com Nutella", "descricao": "Geladinho gourmet de leite ninho com Nutella", "preco": 6.00, "imagem": "geladinho_ninho_nutella.png", "queridinho": True}
+]
+
+
+PRODUTOS_OVOS_PASCOA = [
+    {"id": 26, "nome": "Ovo de Brigadeiro", "descricao": "Ovo de chocolate recheado com brigadeiro", "preco": 40.00, "imagem": "ovo_brigadeiro.png"},
+    {"id": 27, "nome": "Ovo de Ninho com Nutella", "descricao": "Ovo de chocolate recheado com leite ninho e Nutella", "preco": 50.00, "imagem": "ovo_ninho_nutela.png", "queridinho": True},
+    {"id": 28, "nome": "Ovo de Coco", "descricao": "Ovo de chocolate com recheio de coco", "preco": 40.00, "imagem": "ovo_coco.png"},
+    {"id": 29, "nome": "Ovo de Maracujá com Chocolate", "descricao": "Ovo de chocolate com mousse de maracujá", "preco": 45.00, "imagem": "ovo_maracuja.png"},
+    {"id": 30, "nome": "Ovo Ferrero Rocher", "descricao": "Ovo inspirado no Ferrero Rocher", "preco": 55.00, "imagem": "ovo_ferrero.png", "queridinho": True},
+    {"id": 31, "nome": "Ovo Dois Amores", "descricao": "Ovo com chocolate branco e ao leite", "preco": 45.00, "imagem": "ovo_dois_amores.png"},
+    {"id": 32, "nome": "Ovo Laka", "descricao": "Ovo de chocolate branco estilo Laka", "preco": 45.00, "imagem": "ovo_laka.png"}
+]
+
+
+CARDAPIO_DOCERIA = {
+    "Trufas": PRODUTOS_BOMBOM,
+    "brigadeiros": PRODUTOS_BRIGADEIROS,
+   # "geladinhos": PRODUTOS_GELADINHOS,
+   # "ovos_de_pascoa": PRODUTOS_OVOS_PASCOA
+}
